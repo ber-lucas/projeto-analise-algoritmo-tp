69071eaa9daf07e3374fba4ded1225f645675d79
ber-lucas <bernardo.araujo@aluno.ufop.edu.br>
2024-02-04 18:24:58 -0300

    subdistance calculation of planets implemented

--- pd.c
+++ pd.c
@@ -1,15 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define TEST_SIZE_I
-#define TEST_SIZE_J
+int calculatesTheCostOfTheSubdistance(
+  int planet1, int planet2, int subdistance[]
+);
 
-int pd() {
+int pd(int dist[], int n, int k) {
 
-  int memo[TEST_SIZE_I][TEST_SIZE_J];
-  for (int i = 0; i <= TEST_SIZE_I; i++) {
-    for(int j = 0; j <= TEST_SIZE_J; j++) {
-      memo[i][j] = 0;
+  int memo[n + 1][k + 1];
+  for (int i = 0; i <= n; i++) {
+    for(int j = 0; j <= k; j++) {
+      memo[i][j] = -1;
     }
   }
+
+  memo[0][0] = 0;
+  for (int i = 1; i <= n; i++) {
+    memo[i][0] = calculatesTheCostOfTheSubdistance(0, i, dist);
+  }
+
+  return 0;
+}
+
+int calculatesTheCostOfTheSubdistance(
+  int planet1, int planet2, int subdistance[]
+) {
+  int cost = 0;
+
+  for(int i = planet1; i < planet2; i++) {
+    cost += subdistance[i];
+  }
+
+  return cost;
 }
