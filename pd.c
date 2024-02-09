070153988cdcf59e7674224140cd9227cd037502
Breno Rotte <114703393+BrenoRotte@users.noreply.github.com>
2024-02-08 22:18:12 -0300

    Update pd.c
    dynamic programming algorithm update

--- pd.c
+++ pd.c
@@ -5,6 +5,16 @@ int calculatesTheCostOfTheSubdistance(
   int planet1, int planet2, int subdistance[]
 );
 
+int smallerThan(int x, int y);
+
+int biggerThan(int x, int y);
+
+/*
+  Algoritmo de Programação Dinâmica
+    Neste algoritmo utilizamos a heurística utilizada no Algoritmo Guloso para
+    selecionar os k-1 planetas restantes, e então a matriz que armazena estes
+    resultados é utilizada para calcular a solução ótima para este problema.
+*/
 int pd(int dist[], int n, int k) {
 
   int memo[n + 1][k + 1];
@@ -19,12 +29,14 @@ int pd(int dist[], int n, int k) {
     memo[i][0] = calculatesTheCostOfTheSubdistance(0, i, dist);
   }
 
+  /* Calcula os k-1 planetas restantes usando a escolha gulosa como heurística */
   int *travDistance = (int *) calloc(n, sizeof(int));
   int last = n - 1;
   int first = 0;
   for (int i = 1; i < k; i++) {
     int best = -1;
 
+    /* Escolhe o planeta mais distante do último planeta escolhido. */
     for (int j = 0; j < n; j++) {
       if (!travDistance[j] &&
         (best == -1 || dist[j] - dist[last] > dist[best] - dist[last]))
@@ -33,6 +45,11 @@ int pd(int dist[], int n, int k) {
 
     travDistance[best] = 1;
     last = best;
+
+    /* Atualiza o valor de first quando o first planeta é escolhido */
+    if (i == 1) {
+      first = best;
+    }
   }
 
   return 0;
@@ -48,4 +65,14 @@ int calculatesTheCostOfTheSubdistance(
   }
 
   return cost;
+}
+
+// Função para comparar dois números e retornar o menor
+int smallerThan(int x, int y) {
+  return (x < y) ? x : y;
+}
+
+// Função para comparar dois números e retornar o maior
+int biggerThan(int x, int y) {
+  return (x > y) ? x : y;
 }
